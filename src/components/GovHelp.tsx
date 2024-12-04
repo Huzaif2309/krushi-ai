987916c09f3aa26862b3977a9addc8fc5b7d7727
varenyathaker <varenyathaker375@gmail.com>
2024-12-04 22:11:43 +0530

    sd

---
+++ src/components/GovHelp.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+ import ShineBorder from './ui/shine-border'
+const GovHelp = () => {
+  return (
+    <div>
+      <ShineBorder
+      className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl"
+      color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
+    >
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+        Shine Border
+      </span>
+    </ShineBorder>
+    </div>
+  )
+}
+
+export default GovHelp
