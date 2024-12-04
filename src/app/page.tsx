a6b5e343b520d81ec463aeea2b2dc1bb75a54c3a
Samkit Samsukha <samkitsamsukha@gmail.com>
2024-12-05 01:07:04 +0530

    contact us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ import Fertiliser from "@/components/ai/Fertiliser";
 import Particles from "../components/ui/particles";
 import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
-import { CloudSunRain } from "lucide-react";
-
+import { CloudSunRain, Landmark } from "lucide-react";
+import { Search } from "lucide-react";
 export default function Home() {
 	const [color, setColor] = useState("#ffff00");
 	const { resolvedTheme } = useTheme();
@@ -17,8 +17,14 @@ export default function Home() {
 	return (
 		<div className="flex flex-1 flex-col min-h-screen p-4">
 			<div className="relative flex h-[50vh] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background">
-				<h1 className="text-6xl md:text-8xl font-bold text-[#16a34a]">Krushi-AI</h1>
-        <p className="text-xl md:text-3xl mx-10 text-center my-4">Transforming agriculture with precision and power, Krushi AI empowers farmers to achieve unmatched productivity and success. Elevate your farming journey today.</p>
+				<h1 className="text-6xl md:text-8xl font-bold text-[#16a34a]">
+					Krushi-AI
+				</h1>
+				<p className="text-xl md:text-3xl mx-10 text-center my-4">
+					Transforming agriculture with precision and power, Krushi AI empowers
+					farmers to achieve unmatched productivity and success. Elevate your
+					farming journey today.
+				</p>
 				<Particles
 					className="absolute inset-0"
 					quantity={600}
@@ -29,20 +35,35 @@ export default function Home() {
 			</div>
 			<div className="flex flex-col md:flex-row gap-6 h-fit">
 				<div className="w-full md:w-1/3 h-full">
-					<h2 className="text-center text-[#16a34a] font-bold text-2xl mb-2">
-          <CloudSunRain /> Weather Forecast
+					<h2 className="text-center  font-bold text-2xl mb-2">
+						<div className="flex flex-row gap-2 justify-center items-center">
+							<div className="text-[#16a34a]">
+								<CloudSunRain />
+							</div>
+							<div>Weather Forecast</div>
+						</div>
 					</h2>
 					<WeatherApp />
 				</div>
 				<div className="w-full md:w-1/3 h-full">
-					<h2 className="text-center text-[#16a34a] font-bold text-2xl mb-2">
-						Government Initiatives
+        <h2 className="text-center  font-bold text-2xl mb-2">
+						<div className="flex flex-row gap-2 justify-center items-center">
+							<div className="text-[#16a34a]">
+                <Landmark />
+							</div>
+							<div>Government Initiatives</div>
+						</div>
 					</h2>
 					<GovHelp />
 				</div>
 				<div className="w-full md:w-1/3 h-full">
-					<h2 className="text-center text-[#16a34a] font-bold text-2xl mb-2">
-						Fertilizer AI
+        <h2 className="text-center  font-bold text-2xl mb-2">
+						<div className="flex flex-row gap-2 justify-center items-center">
+							<div className="text-[#16a34a]">
+                <Search />
+							</div>
+							<div>Government Initiatives</div>
+						</div>
 					</h2>
 					<Fertiliser />
 				</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,10 +11,13 @@ const Navbar = () => {
 
   return (
     <div className="flex flex-row justify-around items-center p-4 fixed z-10 w-full bg-white dark:bg-black">
-      <div className="font-bold text-3xl flex flex-row justify-center items-center gap-2"><Sprout className="text-[#16a32a] font-extrabold" size={35}/>Krushi-AI</div>
+      <div className="font-bold text-xl md:text-3xl flex flex-row justify-center items-center gap-2"><Sprout className="text-[#16a32a] font-extrabold" size={35}/>Krushi-AI</div>
       <div className="flex flex-row">
         <Button onClick={()=>router.push('/analyze')} variant="outline" className="border-none hover:scale-105 transition-all duration-300">
             Analyze
+        </Button>
+        <Button onClick={()=>router.push('/contact')} variant="outline" className="border-none hover:scale-105 transition-all duration-300">
+            Contact Us
         </Button>
       </div>
       <ModeToggle />
